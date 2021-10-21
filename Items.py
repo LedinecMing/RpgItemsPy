9c7312fcbd7e7284d16c75814b32a3e58e8acfec
Урядов Алексей <63245550+LedinecMing@users.noreply.github.com>
2021-10-21 09:29:37 +0400

    Update Items.py

--- Items.py
+++ Items.py
@@ -27,10 +27,11 @@ class Weapon(Item):
     arg_types: ArgsChecker = {"damage": [typing.Sequence, "raise ValueError(f'Damage is not subclass of typing.Sequence type, got {arg}({type(arg)})')"],
                               "name": [str, "raise ValueError(f'Wrong type for name str or bytes excepted, got {arg}({type(arg)})')"]}
 
-    def __init__(self, damage: DamageInfo, name: typing.AnyStr):
+    def __init__(self, damage: DamageInfo, name: str) -> None:
         if self.check_args({"damage": damage, "name": name}, self.arg_types):
-            self._damage = damage
-            self._name = name
+            self._damage: DamageInfo = damage
+            self._name: str = name
+
     @property
     def damage(self):
         return float(randint(self._damage[0], self._damage[1]))
@@ -39,8 +40,9 @@ class Weapon(Item):
     def name(self):
         return self._name
 
-    def check_args(self, args: typing.Dict[str, typing.Any], arg_types: ArgsChecker):
-        super().check_args(args, self.arg_types)
+    @staticmethod
+    def check_args(args: typing.Dict[str, typing.Any], arg_types: ArgsChecker):
+        super().check_args(args, arg_types)
         if any(not isinstance(damageConfig, int) for damageConfig in args["damage"]):
             raise ValueError(f"Wrong types in damage: {type(args['damage'])}")
         elif len(args["damage"]) != 2:
@@ -52,7 +54,10 @@ class Armor(Item):
     arg_types: ArgsChecker = {"name": [str, "raise ValueError(f'Wrong type for name str or bytes excepted, got {arg}({type(arg)})')"],
                               "defence": [int, 'raise ValueError(f"Defence must be int, got {arg}({type(arg)})")']}
 
-    def __init__(self, defence: int, name: typing.AnyStr):
+    def __init__(self, defence: int, name: str) -> None:
         if super().check_args({"defence": defence, "name": name}, self.arg_types):
-            self._defence = defence
-            self._name = name
+            self._defence: int = defence
+            self._name: str = name
+
+
+stick: Weapon = Weapon([1, 4], "Stick")
